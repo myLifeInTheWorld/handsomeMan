ead667e4f50e59afde9ad759308ffaf0d0189f4d
heruiguang <845356639@qq.com>
2019-07-16 14:36:27 +0800

    第一次提交

---
+++ src/main/java/com/coder/mamabike/MamabikeApplication.java
@@ -0,0 +1,13 @@
+package com.coder.mamabike;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MamabikeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MamabikeApplication.class, args);
+	}
+
+}
